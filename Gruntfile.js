ee0c287041326e36dd26af25b723c9742601a734
Bernardo Pacheco <bernardo.wpacheco@gmail.com>
2014-10-22 23:53:24 -0200

    chore(grunt): add less processor

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function( grunt ) {
+  'use strict';
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  var config;
+
+  config = {
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    meta: {
+      banner: '/*!\n' +
+          ' * <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+          ' * <%= pkg.author %>\n' +
+          ' * <%= pkg.description %>\n' +
+          ' * <%= pkg.url %>\n' +
+          ' * Copyright <%= pkg.copyright %>. <%= pkg.license %> licensed.\n' +
+          ' */\n'
+    },
+
+    clean: [ 'assets' ],
+
+    less: {
+      build: {
+        src: 'src/css/site.less',
+        dest: 'assets/site.css'
+      },
+      dist: {
+        src: 'assets/site.css',
+        dest: 'assets/site.css',
+        options: {
+          banner: '<%= meta.banner %>',
+          cleancss: true,
+          compress: true
+        }
+      }
+    }
+  };
+
+  grunt.initConfig( config );
+
+  grunt.registerTask( 'build', [ 'clean', 'less:build' ] );
+  grunt.registerTask( 'dist', [ 'less:dist' ] );
+  grunt.registerTask( 'default', [ 'build', 'dist' ] );
+};
