3504772080c2a27c2a65450b7eeda818a26e8759
Bernardo Pacheco <bernardo.wpacheco@gmail.com>
2014-11-07 09:11:15 -0200

    feat(share): add Facebook share button

--- assets/scripts.js
+++ assets/scripts.js
@@ -26,4 +26,30 @@
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
   })();
 
+  // Facebook share
+
+  window.fbAsyncInit = function() {
+
+    FB.init({
+      appId: '730635297031262',
+      xfbml: true,
+      version: 'v2.2'
+    });
+  };
+
+  (function( d, s, id ) {
+
+    var js,
+      fjs = d.getElementsByTagName( s )[ 0 ];
+
+    if ( d.getElementById(id) ) {
+      return;
+    }
+
+    js = d.createElement( s );
+    js.id = id;
+    js.src = '//connect.facebook.net/en_US/sdk.js';
+    fjs.parentNode.insertBefore( js, fjs );
+  }( document, 'script', 'facebook-jssdk' ));
+
 })( this, document );
